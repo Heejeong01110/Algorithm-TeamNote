3353dbbd3c3084de0ced259456a1373daed26c48
Heejeong01110 <123rkdrkd@naver.com>
2022-01-03 17:46:20 +0900

    재귀/정답

--- 프로그래머스/Solution60057.java
+++ 프로그래머스/Solution60057.java
@@ -3,68 +3,40 @@ import java.util.*;
 
 public class Solution60057 {
     public static void main(String[] args) throws IOException {
-        String s = "xababcdcdababcdcd";
+        String s = "ababcdcdababcdcd";
         int answer = s.length();
-        
-        Integer length = s.length();
-        String changeStr;
-        while (length > 0) {
-            changeStr = findReply(s, length);
-            answer = Math.min(answer, changeStr.length());
-            length--;
-        }
 
+        Integer len;
+        for (int i = 1; i < (s.length() / 2) + 1; i++) { // 전체 글자의 절반이 최대 반복 횟수이기 때문에 /2, 1을 고려해서 +1
+            len = recursion(s, i).length();
+            answer = Math.min(answer, len);
+        }
 
         System.out.println(answer);
     }
 
-    private static String findReply(String s, Integer length) {
-        Integer count = 1;
-        String replyStr = "";
-        String remainStr = s;
-        String returnStr = "";
-        while (true) {
-            if (replyStr.length() == 0) {
-                replyStr = remainStr.substring(0, length);
-                remainStr = remainStr.substring(length, s.length());
-                count = 1;
-
-            } else if (replyStr.length() > remainStr.length()) {
-                if (count > 1) {
-                    returnStr += count + replyStr;
-                } else {
-                    returnStr += replyStr;
-                }
-                returnStr += remainStr;
-                return returnStr;
-
-            } else if (replyStr.equals(remainStr.substring(0, length))) {
-                remainStr = remainStr.substring(length, remainStr.length());
-                count++; // 계속 검사
-            } else {
-                if (count > 1) {
-                    returnStr += count + replyStr;
-                } else {
-                    returnStr += replyStr;
-                }
-                replyStr = remainStr.substring(0, length);
-                remainStr = remainStr.substring(length, remainStr.length());
-                count = 1;
-            }
-
-            // System.out.println(" 반복중 : " + replyStr + " , " + remainStr);
-            if (remainStr.length() <= 0) {
-                if (count > 1) {
-                    returnStr += count + replyStr;
-                } else {
-                    returnStr += replyStr;
-                }
-                returnStr += remainStr;
-                // System.out.println("최종 : " + returnStr);
-                return returnStr;
-            }
-
+    private static String recursion(String remainStr, Integer strLength) {
+        if (remainStr.length() <= strLength) {
+            return remainStr;
         }
+
+        String replyStr = remainStr.substring(0, strLength);
+        remainStr = remainStr.substring(strLength, remainStr.length());
+        Integer count = 1;
+
+        while (remainStr.length() > 0) {
+            if (!remainStr.startsWith(replyStr)) {
+                break;
+            }
+
+            count++;
+            remainStr = remainStr.substring(strLength, remainStr.length());
+        }
+
+        if (count == 1) {
+            return replyStr + recursion(remainStr, strLength);
+        }
+        return count + replyStr + recursion(remainStr, strLength);
     }
 
 }
