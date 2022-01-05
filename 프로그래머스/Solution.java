2a1b0bddc3530c535de74be6b1e910898e2305e4
Heejeong01110 <123rkdrkd@naver.com>
2022-01-05 18:53:40 +0900

    템플릿 형식 수정

--- 프로그래머스/Solution.java
+++ 프로그래머스/Solution.java
@@ -1,19 +1,22 @@
-import java.io.*;
 import java.util.*;
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        ArrayList<String[]> operationss = new ArrayList<>();
+    public static void main(String[] args) {
+        int[] n = { 2, 2 };
+        String[][] data = { { "N~F=0", "R~T>2" }, { "M~C<2", "C~M>1" } };
+        int[] answer = { 3648, 0 };
 
-
-
-        for (String[] operations : operationss) {
-            System.out.println(Arrays.toString(solution(operations)));
+        for (int i = 0; i < answer.length; i++) {
+            if (solution(n[i], data[i]) == answer[i]) {
+                System.out.println("O");
+            } else {
+                System.out.println("X");
+            }
         }
     }
 
-    private static int[] solution(String[] operations) {
-        int[] answer = { 0, 0 };
+    private static int solution(int n, String[] data) {
+        int answer = 0;
         return answer;
     }
 }
