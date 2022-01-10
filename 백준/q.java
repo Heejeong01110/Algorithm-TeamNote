621884c3ba17bd174fed93f2cd91f38657359e70
Heejeong01110 <123rkdrkd@naver.com>
2022-01-10 20:59:05 +0900

    단순 문제/정답

---
+++ 백준/q.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class q {
+    static int[][] fibo = new int[41][2];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+
+}
