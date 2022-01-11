ea32bdd2c34486fa55babc80cde34b1a385d2151
Heejeong01110 <123rkdrkd@naver.com>
2022-01-11 18:04:23 +0900

    구현/정답

--- 백준/q.java
+++ 백준/q.java
@@ -6,6 +6,9 @@ public class q {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Integer N = Integer.parseInt(st.nextToken());
+        Integer M = Integer.parseInt(st.nextToken());
 
         br.close();
     }
