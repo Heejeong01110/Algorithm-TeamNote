47b1093fdceb311101d2caf88f4436be29853058
Heejeong01110 <123rkdrkd@naver.com>
2022-01-17 13:33:54 +0900

    템플릿

--- java-algorithm/src/main/java/com/example/baekjoon/q.java
+++ java-algorithm/src/main/java/com/example/baekjoon/q.java
@@ -5,12 +5,20 @@ import java.util.*;
 
 public class q {
 
+    private static Integer N;
+    private static Integer M;
+
     public static void main(String[] args) throws IOException {
+        inputData();
+    }
+
+    private static void inputData() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        Integer N = Integer.parseInt(st.nextToken());
-        Integer M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
         br.close();
     }
