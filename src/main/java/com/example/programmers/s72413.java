c1519aed37ac0114b5f9af85917e80fef2fadc84
Heejeong01110 <123rkdrkd@naver.com>
2022-01-26 18:59:35 +0900

    합승 택시 요금/정답/다익스트라

--- src/main/java/com/example/programmers/s72413.java
+++ src/main/java/com/example/programmers/s72413.java
@@ -1,7 +1,6 @@
 package com.example.programmers;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.PriorityQueue;
@@ -9,7 +8,7 @@ import java.util.PriorityQueue;
 //합승 택시 요금
 public class s72413 {
 
-  private static HashMap<Integer, ArrayList<CustomVextor>> nodes;
+  private static HashMap<Integer, ArrayList<CustomVextor>> nodes; //key : 인덱스값, value : 이웃한 인덱스
 
   public static int solution(int n, int s, int a, int b, int[][] fares) {
     //n : 지점의 수 1 ~ n
@@ -27,58 +26,20 @@ public class s72413 {
     }
 
     int[] startDijk = createDijkstraList(s);
-
+    int[] aDijk = createDijkstraList(a);
+    int[] bDijk = createDijkstraList(b);
 
     Integer answer = Integer.MAX_VALUE;
-    for(int i=1;i<=n;i++){
-      int totalCost = startDijk[i] + dijkstra(i,a) + dijkstra(i,b);
+    for (int i = 1; i <= n; i++) {
+      int totalCost = startDijk[i] + aDijk[i] + bDijk[i];
       answer = Math.min(answer, totalCost);
     }
-
-
 
     return answer;
   }
 
-  static int dijkstra(int start, int end) {
-    int[] cost = new int[nodes.size()+1];
-    PriorityQueue<CustomVextor> minCostQueue = new PriorityQueue<>(
-        Comparator.comparingInt(o -> o.cost));
-
-    Arrays.fill(cost, Integer.MAX_VALUE);
-
-    cost[start] = 0;
-    minCostQueue.add(new CustomVextor(start, 0));
-
-    while (!minCostQueue.isEmpty()) {
-      //1. 최소 비용의 노드 선택
-      CustomVextor currentVextor = minCostQueue.poll(); //기준점에서부터의 길이가 저장됨. 새로 생성하는 벡터
-      int currentIndex = currentVextor.index;
-
-      if (currentIndex == end) { //end 지점 도착시 함수 종료
-        return cost[currentIndex];
-      }
-
-      if (cost[currentIndex] < currentVextor.cost) { //스킵
-        continue;
-      }
-
-      //2. 기준점 이웃 노드 중 최소값 구하기
-      for (int j = 0; j < nodes.get(currentIndex).size(); j++) {
-        CustomVextor connectVex = nodes.get(currentIndex).get(j); //커넥트 노드 선택
-
-        if (cost[connectVex.index] > currentVextor.cost + connectVex.cost) {
-          cost[connectVex.index] = currentVextor.cost + connectVex.cost;
-          // 갱신된 경우에만 큐에 넣는다.
-          minCostQueue.add(new CustomVextor(connectVex.index, cost[connectVex.index]));
-        }
-      }
-    }
-    return cost[end];
-  }
-
   static int[] createDijkstraList(int start) {
-    int[] cost = new int[nodes.size()+1];
+    int[] cost = new int[nodes.size() + 1];
     PriorityQueue<CustomVextor> minCostQueue = new PriorityQueue<>(
         Comparator.comparingInt(o -> o.cost));
 
