70ca2b73e6a0f7c6c9a3b855c4dc5b98b1740b1f
Heejeong01110 <123rkdrkd@naver.com>
2022-02-28 14:23:23 +0900

    이중리스트 정렬

--- src/main/java/com/example/algorithm/sortAndSearch/sort/IntegerListSort.java
+++ src/main/java/com/example/algorithm/sortAndSearch/sort/IntegerListSort.java
@@ -3,12 +3,22 @@ package com.example.algorithm.sortAndSearch.sort;
 import java.util.Arrays;
 
 public class IntegerListSort {
+
   public static void main(String[] args) {
-    int[] array = {4,6,2,345,8,76,243,6,43,44,4,7};
+    int[] array = {4, 6, 2, 345, 8, 76, 243, 6, 43, 44, 4, 7};
     Arrays.sort(array);
 
     for (int j : array) {
       System.out.println(j);
     }
+
+    int[][] map = {{1, 2}, {1, 4}, {5, 1}, {6, 4}, {3, 2}, {7, 10}, {10, 10}};
+
+    Arrays.sort(map, (o1, o2) -> {
+      if (o1[0] == o2[0]) {
+        return o1[1] - o2[1]; //오름차순
+      }
+      return o1[0] - o2[0];//오름차순
+    });
   }
 }
---
+++ src/main/java/com/example/baekjoon/q11000.java
@@ -0,0 +1,2 @@
+package com.example.baekjoon;public class q11000 {
+}
