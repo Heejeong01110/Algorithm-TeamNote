87e76076820c17adbec2b6ac3a943169c5bf0f22
Heejeong01110 <123rkdrkd@naver.com>
2022-03-28 20:09:02 +0900

    정렬 상세

--- src/main/java/com/example/algorithm/sortAndSearch/sort/PriorityQueueSort.java
+++ src/main/java/com/example/algorithm/sortAndSearch/sort/PriorityQueueSort.java
@@ -5,11 +5,19 @@ import java.util.PriorityQueue;
 
 public class PriorityQueueSort {
 
+
+
   public static void main(String[] args) {
     PriorityQueue<CustomVextor> minCostQueue = new PriorityQueue<>(
         Comparator.comparingInt(o -> o.cost));
-  }
 
+    PriorityQueue<CustomVextor> priorityQueue = new PriorityQueue<>((o1, o2) -> {
+      if (o1.cost == o2.cost) { //오름차순
+        return o1.index - o2.index;
+      }
+      return o1.cost - o2.cost;
+    });
+  }
 
   static class CustomVextor {
 
