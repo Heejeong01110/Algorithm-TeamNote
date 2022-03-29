c5a75f1fedbe7d6174a91b19afd2af2347b90a7d
Heejeong01110 <123rkdrkd@naver.com>
2022-03-30 03:39:03 +0900

    N-Queen/정답/dfs

---
+++ src/main/java/com/example/programmers/s12952.java
@@ -0,0 +1,54 @@
+package com.example.programmers;
+
+import java.util.Arrays;
+
+public class s12952 {
+
+  private static int result;
+  private static int width;
+
+  public static int solution(int n) {
+    result = 0;
+    width = n;
+
+    for (int i = 0; i < width; i++) {
+      int[] queenInfo = new int[width];
+      Arrays.fill(queenInfo, -1);
+      queenInfo[0] = i;
+      dfs(queenInfo, 1);
+
+    }
+
+    return result;
+  }
+
+  private static void dfs(int[] queenInfo, int row) {
+    if (row == width) {
+      result++;
+      return;
+    }
+
+    for (int i = 0; i < width; i++) {
+      queenInfo[row] = i;
+      if (isPossible(row, queenInfo)) {
+        dfs(queenInfo, row + 1);
+      }
+    }
+
+  }
+
+
+  private static boolean isPossible(int row, int[] queenInfo) {
+    for (int i = 0; i < row; i++) {
+      if (queenInfo[i] == queenInfo[row]) {
+        return false;
+      }
+      if (Math.abs(row - i) == Math.abs(queenInfo[row] - queenInfo[i])) {
+        return false;
+      }
+
+    }
+
+    return true;
+  }
+}
---
+++ src/test/java/com/example/programmers/s12952Test.java
@@ -0,0 +1,13 @@
+package com.example.programmers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class s12952Test {
+
+  @Test
+  void solution() {
+    assertEquals(2, s12952.solution(4));
+  }
+}
