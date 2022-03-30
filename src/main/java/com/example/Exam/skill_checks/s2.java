c897e8c07f281736c2adde3e839044fce4766998
Heejeong01110 <123rkdrkd@naver.com>
2022-03-30 17:16:54 +0900

    오답

--- src/main/java/com/example/Exam/skill_checks/s2.java
+++ src/main/java/com/example/Exam/skill_checks/s2.java
@@ -1,139 +1,53 @@
 package com.example.Exam.skill_checks;
 
 import java.util.ArrayDeque;
+import java.util.Objects;
 import java.util.Queue;
 
-//카카오배 로봇경진대회
+//보드게임
 public class s2 {
 
-  private static int width;
+  public int solution(int[][] board, int[] aloc, int[] bloc) {
+    //이동 가능한 칸의 갯수가 홀, 짝인지에 따라 항상 이기는 플레이어가 결정됨
+    //짝 : B, 홀 : A
 
-  public static int solution(int[][] board) {
-    width = board.length;
-    boolean[][][] visited = new boolean[width][width][2]; // //0 : 가로, 1 : 세로
-    Queue<Node> queue = new ArrayDeque<>();
-    queue.add(new Node(0, 0, 0));
-    visited[0][0][0] = true;
+    Queue<Node> winner = new ArrayDeque<>();
+    Queue<Node> looser = new ArrayDeque<>();
 
-    Node[] moveNodes = new Node[8];
-    for (int i = 0; i < 8; i++) {
-      moveNodes[i] = new Node(0, 0, 0);
-    }
+    //지정하는 로직 필요
+    winner.add(new Node(aloc[0], aloc[1]));
+    looser.add(new Node(bloc[0], bloc[1]));
 
-    int count = 0;
-    while (!queue.isEmpty()) {
-      int size = queue.size();
 
-      for (int i = 0; i < size; i++) {
-        Node now = queue.poll();
-        if (isEnd(now)) {
-          return count;
-        }
-
-        if (now.direct == 0) {
-          moveNodes[0].setInfo(now.row, now.col + 1, 0);
-          moveNodes[1].setInfo(now.row, now.col - 1, 0);
-          moveNodes[2].setInfo(now.row, now.col + 1, 1);
-          moveNodes[3].setInfo(now.row - 1, now.col + 1, 1);
-          moveNodes[4].setInfo(now.row, now.col, 1);
-          moveNodes[5].setInfo(now.row - 1, now.col, 1);
-          moveNodes[6].setInfo(now.row + 1, now.col, 0);//위
-          moveNodes[7].setInfo(now.row - 1, now.col, 0);//아래
-        } else {
-          moveNodes[0].setInfo(now.row + 1, now.col, 1);
-          moveNodes[1].setInfo(now.row - 1, now.col, 1);
-          moveNodes[2].setInfo(now.row + 1, now.col, 0);
-          moveNodes[3].setInfo(now.row, now.col, 0);
-          moveNodes[4].setInfo(now.row + 1, now.col - 1, 0);
-          moveNodes[5].setInfo(now.row, now.col - 1, 0);
-          moveNodes[6].setInfo(now.row, now.col + 1, 1);//좌
-          moveNodes[7].setInfo(now.row, now.col - 1, 1);//우
-        }
-
-        for (int j = 0; j < 8; j++) {
-          if (check(board, moveNodes[j]) && canTurn(now, moveNodes[j], board)
-              && !visited[moveNodes[j].row][moveNodes[j].col][moveNodes[j].direct]) {
-            Node move = new Node(moveNodes[j].row, moveNodes[j].col, moveNodes[j].direct);
-            visited[moveNodes[j].row][moveNodes[j].col][moveNodes[j].direct] = true;
-            queue.add(move);
-          }
-        }
-      }
-
-      count++;
-    }
-
-    return count;
+    int answer = -1;
+    return answer;
   }
 
-  private static boolean canTurn(Node now, Node moveNode, int[][] board) {
-    if (now.direct == moveNode.direct) {
-      return true;
-    }
-
-    if (now.row == moveNode.row && now.col == moveNode.col) {
-      if (board[now.row + 1][now.col + 1] == 1) {
-        return false;
-      }
-    } else if (now.row == moveNode.row) {
-      int checkCol = Math.min(now.col, moveNode.col);
-      if (board[now.row + 1][checkCol] == 1) {
-        return false;
-      }
-    } else if (now.col == moveNode.col) {
-      int checkRow = Math.min(now.row, moveNode.row);
-      if (board[checkRow][now.col + 1] == 1) {
-        return false;
-      }
-    } else {
-      int checkRow = Math.min(now.row, moveNode.row);
-      int checkCol = Math.min(now.col, moveNode.col);
-      if (board[checkRow][checkCol] == 1) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  private static boolean check(int[][] board, Node node) {
-    int nearR = node.direct == 0 ? node.row : node.row + 1;
-    int nearC = node.direct == 0 ? node.col + 1 : node.col;
-    if (node.row >= 0 && node.row < width && node.col >= 0 && node.col < width
-        && nearR >= 0 && nearR < width && nearC >= 0 && nearC < width) {
-      if (board[node.row][node.col] == 0 && board[nearR][nearC] == 0) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  private static boolean isEnd(Node now) {
-    if (now.row == width - 2 && now.col == width - 1 && now.direct == 1) {
-      return true;
-    }
-    if (now.row == width - 1 && now.col == width - 2 && now.direct == 0) {
-      return true;
-    }
-    return false;
-  }
-
-  private static class Node {
-
+  private static class Node{
     int row;
     int col;
-    int direct;
 
-    public Node(int row, int col, int direct) {
+    public Node(int row, int col) {
       this.row = row;
       this.col = col;
-      this.direct = direct; //0 : 가로, 1 : 세로
     }
 
-    public void setInfo(int row, int col, int direct) {
-      this.row = row;
-      this.col = col;
-      this.direct = direct; //0 : 가로, 1 : 세로
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) {
+        return true;
+      }
+      if (!(o instanceof Node)) {
+        return false;
+      }
+      Node node = (Node) o;
+      return row == node.row && col == node.col;
     }
 
+    @Override
+    public int hashCode() {
+      return Objects.hash(row, col);
+    }
   }
+
 }
