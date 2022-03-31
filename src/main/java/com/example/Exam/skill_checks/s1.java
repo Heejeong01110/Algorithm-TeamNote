31aa9cee07bc7340e5eab07e61f223719b027924
Heejeong01110 <123rkdrkd@naver.com>
2022-03-31 21:34:17 +0900

    lv3 test

--- src/main/java/com/example/Exam/skill_checks/s1.java
+++ src/main/java/com/example/Exam/skill_checks/s1.java
@@ -1,36 +1,54 @@
 package com.example.Exam.skill_checks;
 
-import java.util.Arrays;
-
-//입국심사
+//추석트래픽
 public class s1 {
 
-  public static long solution(int n, int[] times) {
-    Arrays.sort(times);
-    long answer = 0;
+  public static int solution(String[] lines) {
+    int[][] logs = new int[lines.length][2];
 
-    long start = 0;
-    long end = (long) times[times.length - 1] * n; //6
-    long mid = 0;
+    for (int i = 0; i < lines.length; i++) {
+      String[] strs = lines[i].split(" ");
 
-    while (start <= end) {
-      mid = (start + end) / 2; //30
+      int end = getTime(strs[1]);
+      int range = getTime(strs[2]);
 
-      long sum = 0;
-      for (long time : times) {
-        sum += mid / time; //mid 시간까지 사용한 사람 수
+      logs[i][0] = end - range + 1;
+      logs[i][1] = end;
+      System.out.println(logs[i][0]+" "+logs[i][1]);
+    }
+
+    int currentCount = 0;
+    int maxCount = 0;
+
+    for (int i = 0; i < logs.length; i++) {
+      currentCount = 0;
+      for (int j = i; j < logs.length; j++) {
+        if (logs[i][1] + 1000 > logs[j][0]) { //1초 범위 내에서 시작하는 로그가 몇개인지 체크
+          currentCount++;
+        }
+
+
       }
 
-      if (sum >= n) {
-        answer = mid;
-        end = mid - 1;
-      } else {
-        start = mid + 1;
-      }
+      maxCount = Math.max(maxCount, currentCount);
 
     }
 
-    return answer;
+    return maxCount;
+  }
+
+  private static int getTime(String str) {
+    String s = str.replace(".", ":").replace("s", "");
+
+    String[] times = s.split("\\:");
+
+    if (times.length == 2) {
+      return Integer.parseInt(times[0]) * 1_000 + Integer.parseInt(times[1]);
+    } else if (times.length == 1) {
+      return Integer.parseInt(times[0]) * 1_000;
+    }
+    return Integer.parseInt(times[0]) * 3600_000 + Integer.parseInt(times[1]) * 60_000
+        + Integer.parseInt(times[2]) * 1_000 + Integer.parseInt(times[3]);
   }
 
 }
--- src/main/java/com/example/Exam/skill_checks/s2.java
+++ src/main/java/com/example/Exam/skill_checks/s2.java
@@ -1,53 +1,57 @@
 package com.example.Exam.skill_checks;
 
-import java.util.ArrayDeque;
-import java.util.Objects;
-import java.util.Queue;
+import java.util.ArrayList;
 
-//보드게임
+//사칙연산
 public class s2 {
 
-  public int solution(int[][] board, int[] aloc, int[] bloc) {
-    //이동 가능한 칸의 갯수가 홀, 짝인지에 따라 항상 이기는 플레이어가 결정됨
-    //짝 : B, 홀 : A
-
-    Queue<Node> winner = new ArrayDeque<>();
-    Queue<Node> looser = new ArrayDeque<>();
-
-    //지정하는 로직 필요
-    winner.add(new Node(aloc[0], aloc[1]));
-    looser.add(new Node(bloc[0], bloc[1]));
-
-
-    int answer = -1;
-    return answer;
-  }
-
-  private static class Node{
-    int row;
-    int col;
-
-    public Node(int row, int col) {
-      this.row = row;
-      this.col = col;
+  public static int solution(int N, int number) {
+    ArrayList<Integer>[] dp = new ArrayList[9];
+    for (int i = 0; i < 9; i++) {
+      dp[i] = new ArrayList<>();
     }
 
-    @Override
-    public boolean equals(Object o) {
-      if (this == o) {
-        return true;
+    dp[1].add(N);
+
+    if (number == N) {
+      return 1;
+    }
+
+    Integer[] middleCalc = new Integer[6];
+    for (int i = 2; i < 9; i++) {
+
+      dp[i].add(dp[i - 1].get(0) * 10 + N);
+      if (dp[i].get(0) == number) {
+        return i;
       }
-      if (!(o instanceof Node)) {
-        return false;
+      for (int j = 1; j <= i / 2; j++) {
+        int aIndex = j;
+        int bIndex = i - j;
+
+        for (int a = 0; a < dp[aIndex].size(); a++) {
+          for (int b = 0; b < dp[bIndex].size(); b++) {
+            middleCalc[0] = dp[aIndex].get(a) + dp[bIndex].get(b);
+            middleCalc[1] = dp[aIndex].get(a) - dp[bIndex].get(b);
+            middleCalc[2] = dp[aIndex].get(a) * dp[bIndex].get(b);
+            middleCalc[3] = dp[aIndex].get(a) / dp[bIndex].get(b);
+            middleCalc[4] = dp[bIndex].get(b) - dp[aIndex].get(a);
+            middleCalc[5] = dp[bIndex].get(b) / dp[aIndex].get(a);
+
+            for (int k = 0; k < 6; k++) {
+              if (middleCalc[k] == number) {
+                return i;
+              }
+              if (middleCalc[k] >= 1 && middleCalc[k] <= 32000 && !dp[i].contains(middleCalc[k])) {
+                dp[i].add(middleCalc[k]);
+              }
+            }
+          }
+        }
       }
-      Node node = (Node) o;
-      return row == node.row && col == node.col;
+
     }
 
-    @Override
-    public int hashCode() {
-      return Objects.hash(row, col);
-    }
+    return -1;
   }
 
 }
