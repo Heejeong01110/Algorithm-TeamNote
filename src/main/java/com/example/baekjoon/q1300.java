8fa72223ff1eda9e97a98e342e4ccebf2d770161
Heejeong01110 <123rkdrkd@naver.com>
2022-04-29 22:59:03 +0900

    K번째 수/정답/다시 풀어보기

---
+++ src/main/java/com/example/baekjoon/q1300.java
@@ -0,0 +1,57 @@
+package com.example.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class q1300 {
+
+  private static final int max = 1_000_000_000;
+  private static int N;
+  private static int k;
+
+  public static void main(String[] args) throws IOException {
+    run();
+  }
+
+  public static void run() throws IOException {
+    inputData();
+    output(Solution());
+  }
+
+  private static void output(long result) {
+    System.out.print(result);
+  }
+
+  private static void inputData() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    N = Integer.parseInt(br.readLine());
+    k = Integer.parseInt(br.readLine());
+    br.close();
+  }
+
+  private static long Solution() {
+    long mid;
+    long left = 1;
+    long right = k;
+
+    while (right > left) {
+      mid = (right + left) / 2;
+
+      long count = 0;
+      for (int i = 1; i <= N; i++) {
+        count += Math.min(mid / i, N);
+      }
+
+      if (count >= k) {
+        right = mid;
+      } else {
+        left = mid + 1;
+      }
+    }
+
+    return left;
+  }
+
+}
