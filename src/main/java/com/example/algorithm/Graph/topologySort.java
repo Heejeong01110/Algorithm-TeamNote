079461629dd41800c0e9498450a2b8962b83754c
Heejeong01110 <123rkdrkd@naver.com>
2022-06-20 20:18:38 +0900

    위상정렬 최소 소요시간

--- src/main/java/com/example/algorithm/Graph/topologySort.java
+++ src/main/java/com/example/algorithm/Graph/topologySort.java
@@ -11,10 +11,12 @@ public class topologySort {
   private static ArrayList<Integer>[] nodes;
   private static int[] degree;
   private static int N;
+  private static int[] times;
 
   public static void main(String[] args) {
     int[][] fares = new int[][]{{1, 4}, {4, 3}, {6, 2}, {2, 5},
         {5, 4}, {2, 3}};
+    times = new int[]{5, 10, 4, 7, 15, 2};
     N = 6;
     degree = new int[N + 1];
 
@@ -37,7 +39,8 @@ public class topologySort {
 
     Queue<Integer> queue = new ArrayDeque<>();
     Queue<Integer> result = new ArrayDeque<>();
-
+    int[] resultValue = new int[N + 1]; //최소 소요시간
+    
     // 차수가 0인 정점 큐에 넣기
     for (int i = 1; i <= N; i++) {
       if (degree[i] == 0) {
@@ -52,6 +55,7 @@ public class topologySort {
       for (int next : nodes[now]) {
         degree[next]--;
 
+        resultValue[next] = Math.max(resultValue[next], resultValue[now] + times[now]);
         if (degree[next] == 0) {
           queue.add(next);
         }
