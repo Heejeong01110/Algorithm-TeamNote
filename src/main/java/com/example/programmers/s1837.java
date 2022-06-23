3043b49dee6539e23f72d2da096a1105a9a7c47c
Heejeong01110 <123rkdrkd@naver.com>
2022-06-23 20:19:25 +0900

    GPS/정답/dp

--- src/main/java/com/example/programmers/s1837.java
+++ src/main/java/com/example/programmers/s1837.java
@@ -1,17 +1,15 @@
 package com.example.programmers;
 
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Queue;
+import java.util.Arrays;
 
 public class s1837 {
 
+  private static final int INF = 10000000;
   private static ArrayList<Integer>[] nodes;
   private static int result;
 
   public static int solution(int n, int m, int[][] edge_list, int k, int[] gps_log) {
-    result = Integer.MAX_VALUE;
-
     nodes = new ArrayList[n + 1];
     for (int i = 1; i <= n; i++) {
       nodes[i] = new ArrayList<>();
@@ -22,81 +20,33 @@ public class s1837 {
       nodes[edge_list[i][1]].add(edge_list[i][0]);
     }
 
-    for (int c = 0; c <= k - 2; c++) { //c : 바꿀 데이터 갯수
-      comb(gps_log, new boolean[k], 1, 0, c);
+    int[][] dp = new int[k][n + 1];
+    for (int i = 0; i < k; i++) {
+      Arrays.fill(dp[i], INF);
+    }
+    dp[0][gps_log[0]] = 0;
+
+    for (int i = 1; i < k; i++) {
+      for (int j = 1; j <= n; j++) {
+        //dp[i][j] : i번째 순서에서 j번째 노드에 위치하기 위해 변경한 갯수
+        //1. 이전 순서에서 이번 순서로 이동하는 경우 중(제자리, 연결 노드에서 이동) 가장 적은 변경갯수를 가지는 수를 구함
+        dp[i][j] = Math.min(dp[i][j], dp[i - 1][j]);//이동하지 않을 경우
+
+        for (int l = 0; l < nodes[j].size(); l++) { //연결된 노드에서 이동하는 경우 중 최소값
+          dp[i][j] = Math.min(dp[i][j], dp[i - 1][nodes[j].get(l)]);
+        }
+
+        //2. 현재 노드가 log과 다를 경우 이번 순서에 변경한 것으로 기록
+        if (gps_log[i] != j) {
+          dp[i][j] += 1;
+        }
+      }
     }
 
-    if(result == Integer.MAX_VALUE){
+    if (dp[k - 1][gps_log[k - 1]] >= INF) {
       return -1;
     }
-    return result;
+    return dp[k - 1][gps_log[k - 1]];
   }
 
-  private static void comb(int[] gps_log, boolean[] visited, int start, int depth, int r) {
-    if (result != Integer.MAX_VALUE) {
-      return;
-    }
-
-    if (depth == r) {
-      checkConnection(gps_log, visited, r);
-      return;
-    }
-
-    //출발지, 도착지는 포함 X
-    for (int i = start; i < gps_log.length - 1; i++) {
-      if (!visited[i]) {
-        visited[i] = true;
-        comb(gps_log, visited, i + 1, depth + 1, r);
-        visited[i] = false;
-      }
-    }
-  }
-
-  private static void checkConnection(int[] gps_log, boolean[] changeIdx, int changeCnt) {
-    Queue<Integer> queue = new ArrayDeque<>();
-    queue.add(gps_log[0]);
-    int idx = 1;
-
-    while (!queue.isEmpty()) {
-      //중복 제거
-      int size = queue.size();
-      for (int s = 0; s < size; s++) {
-        int now = queue.poll();
-        if (!queue.contains(now)) {
-          queue.add(now);
-        }
-      }
-
-      size = queue.size();
-      for (int s = 0; s < size; s++) {
-        int now = queue.poll();
-
-        if (now == gps_log[gps_log.length - 1] && idx == gps_log.length) {
-          result = changeCnt;
-          return;
-        }
-
-        if (changeIdx[idx]) { //1. 경로를 변경 O인 경우
-          for (int i = 0; i < nodes[now].size(); i++) { //연결 노드
-            int next = nodes[now].get(i);
-            if (next != gps_log[idx]) {
-              queue.add(next);
-            }
-          }
-
-          if (now != gps_log[idx]) {//제자리 대기
-            queue.add(now);
-          }
-
-        } else { //2. 경로 변경 X
-          if (nodes[now].contains(gps_log[idx]) || now == gps_log[idx]) { //연결된 길일 경우만 계속 진행
-            queue.add(gps_log[idx]);
-          }
-        }
-      }
-      idx++;
-    }
-
-
-  }
 }
