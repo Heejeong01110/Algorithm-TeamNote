0747d98cc44a74399c63f42fd95f511c470fd4d6
Heejeong01110 <123rkdrkd@naver.com>
2022-07-01 21:13:29 +0900

    N으로 표현/정답

--- src/main/java/com/example/programmers/s42895.java
+++ src/main/java/com/example/programmers/s42895.java
@@ -4,70 +4,47 @@ import java.util.ArrayList;
 
 public class s42895 {
 
-  private static ArrayList<Integer> list;
-  private static ArrayList<Integer>[] map;
-
   public static int solution(int N, int number) {
-    list = new ArrayList<>();
-    map = new ArrayList[9];
-
-    for (int i = 1; i < 9; i++) {
-      map[i] = new ArrayList<>();
-    }
-    list.add(N);
-    map[1].add(N);
-    if (input(1, number, N)) {
-      return 1; //종료
-    }
-    Integer one;
-    Integer two;
-    int[] calcResult = new int[7];
-    for (int i = 2; i <= 8; i++) {
-      calcResult[0] = 0;
-      for (int j = 0; j < i; j++) {
-        calcResult[0] += N * Math.pow(10, j);
+    ArrayList<Integer>[] nodes = new ArrayList[9];
+    int num = 0;
+    for (int i = 1; i <= 8; i++) {
+      nodes[i] = new ArrayList<>();
+      num = num * 10 + N;
+      if (num == number) {
+        return i;
       }
-      if (calcResult[0] <= 32000 && input(i, number, calcResult[0])) {
-        return i; //종료
-      }
+      nodes[i].add(num);
+    }
 
-      for (int j = 1; j <= i / 2; j++) {
+    for (int s = 2; s <= 8; s++) {
+      for (int idx = 1; idx <= s / 2; idx++) {
 
-        for (int n = 0; n < map[j].size(); n++) {
-          one = map[j].get(n);
-          for (int m = 0; m < map[i - j].size(); m++) {
-            two = map[i - j].get(m);
-            calcResult[1] = one - two;
-            calcResult[2] = one / two;
-            calcResult[3] = one + two;
-            calcResult[4] = one * two;
-            calcResult[5] = two - one;
-            calcResult[6] = two / one;
+        for (Integer o : nodes[idx]) {
+          for (Integer t : nodes[s - idx]) {
+            int[] list = new int[6];
+            list[0] = o + t;
+            list[1] = o - t;
+            list[2] = o * t;
+            list[3] = t != 0 ? o / t : 0;
+            list[4] = t - o;
+            list[5] = o != 0 ? t / o : 0;
 
-            for (int t = 1; t < 7; t++) {
-              if (calcResult[t] >= 1 && calcResult[t] <= 32000 && input(i, number, calcResult[t])) {
-                return i; //종료
+            for (int i = 0; i < 6; i++) {
+              if (list[i] >= 0 && list[i] <= 32000) {
+                if (nodes[s].contains(list[i])) {
+                  continue;
+                }
+                if (list[i] == number) {
+                  return s;
+                }
+                nodes[s].add(list[i]);
               }
             }
-
           }
         }
       }
     }
-
     return -1;
-  }
-
-  private static boolean input(int len, int target, int num) {
-    if (num == target) {
-      return true;
-    }
-
-    if (!list.contains(num)) {
-      list.add(num);
-      map[len].add(num);
-    }
-    return false;
   }
 
 }
