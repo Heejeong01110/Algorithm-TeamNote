133e13c6ed8bfc33095aac9953a708d98b1e2648
Heejeong01110 <123rkdrkd@naver.com>
2023-05-31 20:38:42 +0900

    무지의 먹방 라이브/오답

---
+++ src/test/java/com/example/programmers/s42891Test.java
@@ -0,0 +1,13 @@
+package com.example.programmers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class s42891Test {
+
+  @Test
+  void solution() {
+    assertEquals(1, s42891.solution(new int[]{3, 1, 2}, 5));
+  }
+}
