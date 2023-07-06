3b81f5aa8ba949e84647fe9de330aa5bf33b2f96
Heejeong01110 <123rkdrkd@naver.com>
2023-07-06 17:57:17 +0900

    택배상자/정답/queue

---
+++ src/main/java/com/example/programmers/s131704.java
@@ -0,0 +1,36 @@
+package com.example.programmers;
+
+import java.util.Stack;
+
+public class s131704 {
+
+  public static int solution(int[] order) {
+    Stack<Integer> stack = new Stack<>();
+
+    int boxIdx = 1;
+    int truckNowIdx = 0;
+
+    while (truckNowIdx < order.length) {
+      if (boxIdx == order[truckNowIdx]) {
+        truckNowIdx++;
+        boxIdx++;
+        continue;
+      }
+      
+      if (!stack.isEmpty() && stack.peek() == order[truckNowIdx]) {
+        truckNowIdx++;
+        stack.pop();
+        continue;
+      }
+
+
+      if(boxIdx >order.length){
+        break;
+      }
+      stack.add(boxIdx);
+      boxIdx++;
+    }
+
+    return truckNowIdx;
+  }
+}
---
+++ src/test/java/com/example/programmers/s131704Test.java
@@ -0,0 +1,15 @@
+package com.example.programmers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class s131704Test {
+
+  @Test
+  void solution() {
+    assertEquals(2, s131704.solution(new int[]{4, 3, 1, 2, 5}));
+    assertEquals(5, s131704.solution(new int[]{5,4,3,2,1}));
+
+  }
+}
