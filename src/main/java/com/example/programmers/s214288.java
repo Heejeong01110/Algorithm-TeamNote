753a3319a23da15c7291ce51cbc8fc6697854760
Heejeong01110 <123rkdrkd@naver.com>
2023-07-21 01:08:17 +0900

    상담원 인원/정답/구현

--- src/main/java/com/example/programmers/s214288.java
+++ src/main/java/com/example/programmers/s214288.java
@@ -4,10 +4,9 @@ import java.util.ArrayList;
 import java.util.PriorityQueue;
 
 public class s214288 {
-
   private ArrayList<User>[] users;
-  private int P, K, N;
-  private int answer;
+  private int P, K, N, answer;
+  int[][] mentors;
 
 
   public int solution(int k, int n, int[][] reqs) {
@@ -16,6 +15,8 @@ public class s214288 {
     K = k;
     N = n;
     users = new ArrayList[K + 1];
+    mentors = new int[K + 1][P + 1];
+
     for (int i = 1; i <= K; i++) {
       users[i] = new ArrayList<>();
     }
@@ -26,19 +27,18 @@ public class s214288 {
       users[i].sort((o1, o2) -> o1.start == o2.start ? o1.end - o2.end : o1.start - o2.start);
     }
 
-    int[][] typeMap = new int[K + 1][P + 1];
-    int usedMentor = 0, delaySum = 0;
+    int usedMentor, delaySum;
     PriorityQueue<User> queue;
 
-    for (int kk = 1; kk <= K; kk++) {//kk타입인 참가자 체크
-      int size = users[kk].size();
+    for (int t = 1; t <= K; t++) {// t타입인 참가자 체크
+      int size = users[t].size();
       for (int s = 1; s < size; s++) { //멘토가 s명일 경우 대기시간 구하기
         queue = new PriorityQueue<>((o1, o2) -> o1.end - o2.end);
         usedMentor = 0;
         delaySum = 0;
 
         for (int i = 0; i < size; i++) {
-          User user = users[kk].get(i);
+          User user = users[t].get(i);
 
           while (!queue.isEmpty()) {
             if (queue.peek().end > user.start) {
@@ -48,38 +48,37 @@ public class s214288 {
             queue.poll();
           }
 
-          if (s - usedMentor > 0) {
-            //1. 상담 중이 아닌 멘토가 있을 때
+          if (s - usedMentor > 0) { //1. 상담 중이 아닌 멘토가 있을 때
             usedMentor += 1;
             queue.add(user);
-          } else {//2. 모두 상담중일 경우 -> 대기 후 상담(먼저 상담요청한 참가자 우선)
+          } else { //2. 모두 상담중일 경우
             int delayTime = queue.poll().end - user.start;
             delaySum += delayTime;
             User delayUser = new User(user.type, user.start + delayTime, user.end + delayTime);
             queue.add(delayUser);
           }
         }
-        typeMap[kk][s] = delaySum;
+        mentors[t][s] = delaySum;
       }
     }
 
-    dfs(new int[K + 1], N, 1, typeMap);
+    dfs(new int[K + 1], N, 1);
     return answer;
   }
 
-  private void dfs(int[] result, int remain, int idx, int[][] typeMap) {
+  private void dfs(int[] result, int remain, int idx) {
     if (idx == K) {
       result[idx] = remain;
       int sum = 0;
       for (int i = 1; i <= K; i++) {
-        sum += typeMap[i][result[i]];
+        sum += mentors[i][result[i]];
       }
       answer = Math.min(answer, sum);
       return;
     }
-    for (int i = 1; i < remain - (K - idx); i++) {
+    for (int i = 1; i <= remain - (K - idx); i++) {
       result[idx] = i;
-      dfs(result, remain - result[idx], idx + 1, typeMap);
+      dfs(result, remain - result[idx], idx + 1);
     }
   }
 
