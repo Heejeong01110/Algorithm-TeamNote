15c6b30e8ecbf61c1fe8aaf12320e985d42dc1a5
Heejeong01110 <123rkdrkd@naver.com>
2023-07-21 14:11:45 +0900

    전력망을 둘로 나누기/정답/dfs한번 통과

--- src/main/java/com/example/programmers/s86971.java
+++ src/main/java/com/example/programmers/s86971.java
@@ -5,11 +5,13 @@ import java.util.ArrayList;
 public class s86971 {
 
   private ArrayList<Integer>[] map;
-  private int N;
+  private int N, answer;
+  private boolean[] visited;
 
   public int solution(int n, int[][] wires) {
-    int answer = n;
+    answer = n;
     N = n;
+    visited = new boolean[n + 1];
     map = new ArrayList[n + 1];
     for (int i = 1; i <= n; i++) {
       map[i] = new ArrayList<>();
@@ -19,29 +21,24 @@ public class s86971 {
       map[wire[0]].add(wire[1]);
       map[wire[1]].add(wire[0]);
     }
-
-    for (int[] wire : wires) {
-      answer = Math.min(answer, getCost(wire[0], wire[1]));
-    }
+    dfs(1);
 
     return answer;
   }
 
-  private int getCost(int s, int e) {
-    boolean[] visited = new boolean[N + 1];
-    visited[s] = true;
-    visited[e] = true;
-    return Math.abs(2 * dfs(visited, s) - N);
-  }
+  private int dfs(int idx) {
+    int sum = 1;
+    visited[idx] = true;
 
-  private int dfs(boolean[] visited, int idx) {
-    int answer = 1;
     for (int i = 0; i < map[idx].size(); i++) {
       if (!visited[map[idx].get(i)]) {
         visited[map[idx].get(i)] = true;
-        answer += dfs(visited, map[idx].get(i));
+        sum += dfs(map[idx].get(i));
       }
     }
-    return answer;
+
+    answer = Math.min(answer, Math.abs(2 * sum - N));
+    return sum;
   }
+
 }
