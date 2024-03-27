20830c4997324ecb739c1a2ccfcba79816cd2898
Heejeong01110 <123rkdrkd@naver.com>
2024-03-28 02:25:53 +0900

    행렬과 연산/오답2

--- src/main/java/com/example/programmers/s118670.java
+++ src/main/java/com/example/programmers/s118670.java
@@ -7,13 +7,23 @@ public class s118670 {
   public static int[][] solution(int[][] rc, String[] operations) {
     N = rc.length;
     M = rc[0].length;
+
     int start = 0;
+    int stack = 0;
     for (String operation : operations) {
       if (operation.equals("ShiftRow")) {
+        if (stack != 0) {
+          rc = rotate(rc, start, stack);
+          stack = 0;
+        }
         start = (start + N - 1) % rc.length;
       } else if (operation.equals("Rotate")) {
-        rc = rotate(rc, start);
+        stack += 1;
       }
+    }
+
+    if (stack != 0) {
+      rc = rotate(rc, start, stack);
     }
 
     int[][] answer = new int[N][M];
@@ -27,27 +37,51 @@ public class s118670 {
     return answer;
   }
 
-  private static int[][] rotate(int[][] rc, int start) {
-    int tmp = rc[start][0];
-    for (int i = 0; i < N - 1; i++) {
-      int row = (start + i) % N;
-      int next = (start + i + 1) % N;
-      rc[row][0] = rc[next][0];
+  private static int[][] rotate(int[][] rc, int start, int stack) {
+    int[][] cl = new int[N][M];
+    for (int i = 0; i < N; i++) {
+      cl[i] = rc[i].clone();
     }
-    for (int i = 0; i < M - 1; i++) {
-      int row = (start + N - 1) % N;
-      int col = i;
-      rc[row][col] = rc[row][col + 1];
+
+    int[] now = new int[]{start, 0};
+    boolean tmp = false;
+    while (now[0] != start || now[1] != 0 || !tmp) {
+      tmp = true;
+      int[] copy = getLoc(now, start, stack);
+      rc[now[0]][now[1]] = cl[copy[0]][copy[1]];
+
+      now = getLoc(now, start, 1);
     }
-    for (int i = N - 1; i > 0; i--) {
-      int row = (start + i) % N;
-      int pre = (start + i - 1) % N;
-      rc[row][M - 1] = rc[pre][M - 1];
-    }
-    for (int i = M - 1; i > 0; i--) {
-      rc[start][i] = rc[start][i - 1];
-    }
-    rc[start][1] = tmp;
+
     return rc;
+  }
+
+  private static int[] getLoc(int[] now, int start, int length) {
+    length = length % ((N + M) * 2 - 4); //한바퀴 미만인 갯수로만 돌기
+    int start_max_row = (start + N - 1) % N;
+    int row = now[0];
+    int col = now[1];
+
+    while (length != 0) {
+      if (col == 0 && row != start_max_row) {
+        int move = Math.min(length, (start_max_row + N - row) % N);
+        row = (row + move) % N;
+        length -= move;
+      } else if (row == start_max_row && col != M - 1) {
+        int move = Math.min(length, M - 1 - col);
+        col += move;
+        length -= move;
+      } else if (col == M - 1 && row != start) {
+        int move = Math.min(length, (row + N - start) % N);//윗쪽으로 구해야 함
+        row = (row + N - move) % N;
+        length -= move;
+
+      } else if (row == start) {
+        int move = Math.min(length, col);
+        col -= move;
+        length -= move;
+      }
+    }
+    return new int[]{row, col};
   }
 }
