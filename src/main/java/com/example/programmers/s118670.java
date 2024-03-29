34b492f41cf5b542e010aab8f8d69a1a5c7495ce
Heejeong01110 <123rkdrkd@naver.com>
2024-03-29 19:45:20 +0900

    행렬과 연산/정답

--- src/main/java/com/example/programmers/s118670.java
+++ src/main/java/com/example/programmers/s118670.java
@@ -1,87 +1,56 @@
 package com.example.programmers;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class s118670 {
 
   private static int N, M;
+  private static Deque<Integer> left, right;
+  private static Deque<Deque<Integer>> middle;
 
   public static int[][] solution(int[][] rc, String[] operations) {
     N = rc.length;
     M = rc[0].length;
 
-    int start = 0;
-    int stack = 0;
-    for (String operation : operations) {
-      if (operation.equals("ShiftRow")) {
-        if (stack != 0) {
-          rc = rotate(rc, start, stack);
-          stack = 0;
-        }
-        start = (start + N - 1) % rc.length;
-      } else if (operation.equals("Rotate")) {
-        stack += 1;
+    left = new ArrayDeque<>();
+    right = new ArrayDeque<>();
+    middle = new ArrayDeque<>();
+
+    for (int i = 0; i < N; i++) {
+      left.addLast(rc[i][0]);
+      right.addLast(rc[i][M - 1]);
+      Deque<Integer> mid = new ArrayDeque<>();
+      for (int j = 1; j < M - 1; j++) {
+        mid.addLast(rc[i][j]);
       }
+      middle.addLast(mid);
     }
 
-    if (stack != 0) {
-      rc = rotate(rc, start, stack);
+    for (String operation : operations) {
+      if (operation.equals("ShiftRow")) {
+        middle.addFirst(middle.pollLast());
+        left.addFirst(left.pollLast());
+        right.addFirst(right.pollLast());
+      } else if (operation.equals("Rotate")) {
+        middle.getFirst().addFirst(left.pollFirst());
+        right.addFirst(middle.getFirst().pollLast());
+        middle.getLast().addLast(right.pollLast());
+        left.addLast(middle.getLast().pollFirst());
+      }
     }
 
     int[][] answer = new int[N][M];
     for (int i = 0; i < N; i++) {
-      int r = (start + i) % N;
-      for (int c = 0; c < M; c++) {
-        answer[i][c] = rc[r][c];
+      answer[i][0] = left.pollFirst();
+      answer[i][M - 1] = right.pollFirst();
+      Deque<Integer> now = middle.pollFirst();
+      for (int j = 1; j < M - 1; j++) {
+        answer[i][j] = now.pollFirst();
       }
     }
 
     return answer;
   }
 
-  private static int[][] rotate(int[][] rc, int start, int stack) {
-    int[][] cl = new int[N][M];
-    for (int i = 0; i < N; i++) {
-      cl[i] = rc[i].clone();
-    }
-
-    int[] now = new int[]{start, 0};
-    boolean tmp = false;
-    while (now[0] != start || now[1] != 0 || !tmp) {
-      tmp = true;
-      int[] copy = getLoc(now, start, stack);
-      rc[now[0]][now[1]] = cl[copy[0]][copy[1]];
-
-      now = getLoc(now, start, 1);
-    }
-
-    return rc;
-  }
-
-  private static int[] getLoc(int[] now, int start, int length) {
-    length = length % ((N + M) * 2 - 4); //한바퀴 미만인 갯수로만 돌기
-    int start_max_row = (start + N - 1) % N;
-    int row = now[0];
-    int col = now[1];
-
-    while (length != 0) {
-      if (col == 0 && row != start_max_row) {
-        int move = Math.min(length, (start_max_row + N - row) % N);
-        row = (row + move) % N;
-        length -= move;
-      } else if (row == start_max_row && col != M - 1) {
-        int move = Math.min(length, M - 1 - col);
-        col += move;
-        length -= move;
-      } else if (col == M - 1 && row != start) {
-        int move = Math.min(length, (row + N - start) % N);//윗쪽으로 구해야 함
-        row = (row + N - move) % N;
-        length -= move;
-
-      } else if (row == start) {
-        int move = Math.min(length, col);
-        col -= move;
-        length -= move;
-      }
-    }
-    return new int[]{row, col};
-  }
 }
