d0ea29f6374e4b637b784afc0d9d64e93543a946
Heejeong01110 <123rkdrkd@naver.com>
2024-05-09 17:41:18 +0900

    모노미노도미노 2/정답/시뮬레이션

--- src/main/java/com/example/baekjoon/q20061.java
+++ src/main/java/com/example/baekjoon/q20061.java
@@ -59,16 +59,16 @@ public class q20061 {
       ArrayList<int[]> now = new ArrayList<>();
       now.add(new int[]{in[1], in[2]});
       if (in[0] != 1) {
-        now.add(new int[]{in[1] + shape[in[0]][0], in[2] + shape[in[0]][0]});
+        now.add(new int[]{in[1] + shape[in[0]][0], in[2] + shape[in[0]][1]});
       }
 
       //2. 그린, 블루 방향으로 블럭 넣기
-      int greenMax = addBlock(green, now, true);
-      int blueMax = addBlock(blue, now, false);
+      addBlock(green, now, true);
+      addBlock(blue, now, false);
 
       //3. 한줄이 꽉 찬 경우 찾아서 없애기 -> score 한줄당 1점
-      score += bingo(green, greenMax);
-      score += bingo(blue, blueMax);
+      score += bingo(green);
+      score += bingo(blue);
 
       //4. 오버된 경우 찾아서 지우기
       downBlock(green);
@@ -78,10 +78,10 @@ public class q20061 {
 
     for (int i = 0; i < 4; i++) {
       while (!green.get(i).isEmpty()) {
-        cnt += green.get(i).pollLast();
+        cnt += green.get(i).pollFirst();
       }
       while (!blue.get(i).isEmpty()) {
-        cnt += blue.get(i).pollLast();
+        cnt += blue.get(i).pollFirst();
       }
     }
 
@@ -100,24 +100,26 @@ public class q20061 {
 
     for (int i = 0; i < delete; i++) {
       for (int j = 0; j < 4; j++) {
-        tile.get(j).pollLast();
+        tile.get(j).pollFirst();
       }
     }
 
   }
 
-  private static int bingo(ArrayList<Deque<Integer>> tile, int max) {
+  private static int bingo(ArrayList<Deque<Integer>> tile) {
     int score = 0;
+    int max = 0;
     int[][] tmp = new int[4][6];
 
-    for (int i = 0; i < max; i++) {
+    for (int i = 0; i < 4; i++) {
       int idx = 0;
+      max = Math.max(tile.get(i).size(), max);
       while (!tile.get(i).isEmpty()) {
-        tmp[i][idx++] = tile.get(i).pollLast();
+        tmp[i][idx++] = tile.get(i).pollFirst();
       }
     }
 
-    // 빙고를 찾으면서 빈 부분에 0 넣어주기
+    // 빙고 찾기
     for (int i = 0; i < max; i++) {
       boolean bingo = true;
       for (int j = 0; j < 4; j++) {
@@ -131,40 +133,61 @@ public class q20061 {
         score += 1;
       } else {
         for (int j = 0; j < 4; j++) {
-          tile.get(j).addFirst(tmp[j][i]);
+          tile.get(j).addLast(tmp[j][i]);
         }
       }
     }
+
+    for (int i = 0; i < 4; i++) {
+      while (!tile.get(i).isEmpty()) {
+        if (tile.get(i).peekLast() == 1) {
+          break;
+        }
+        tile.get(i).pollLast();
+      }
+    }
+
     return score;
   }
 
-  private static int addBlock(ArrayList<Deque<Integer>> tile, ArrayList<int[]> now, boolean dir) {
+  private static void addBlock(ArrayList<Deque<Integer>> tile, ArrayList<int[]> now, boolean dir) {
     //dir 0 -> 그대로, 1 -> (x,y)좌표 바꿔서
 
     if (now.size() == 1) { // t == 1일때
       int r = dir ? now.get(0)[0] : now.get(0)[1];
       int c = dir ? now.get(0)[1] : now.get(0)[0];
-      tile.get(c).addFirst(1);
-      return tile.get(c).size();
+      tile.get(c).addLast(1);
+      return;
     }
 
     //1자로 떨어질 때
     if (dir && now.get(0)[1] == now.get(1)[1] ||
         !dir && now.get(0)[0] == now.get(1)[0]) {
       int idx = dir ? now.get(0)[1] : now.get(0)[0];
-      tile.get(idx).addFirst(1);
-      tile.get(idx).addFirst(1);
-      return tile.get(idx).size();
+      tile.get(idx).addLast(1);
+      tile.get(idx).addLast(1);
+      return;
     }
 
-    //ㅡ자로 떨어질때
+    //ㅡ자로 떨어질때 --> 각 위치 중 더 높은 위치를 기준으로 놓여야 함.
     int max = 0;
-    for (int i = 0; i < now.size(); i++) {
+    for (int i = 0; i < now.size(); i++) { //1. 더 높은 위치 찾기
       int idx = dir ? now.get(i)[1] : now.get(i)[0];
-      tile.get(idx).addFirst(1);
       max = Math.max(max, tile.get(idx).size());
     }
-    return max;
+    for (int i = 0; i < now.size(); i++) {
+      int idx = dir ? now.get(i)[1] : now.get(i)[0];
+      if (tile.get(idx).size() == max) {
+        tile.get(idx).addLast(1);
+        continue;
+      }
+
+      while (tile.get(idx).size() < max) {
+        tile.get(idx).addLast(0);
+      }
+      tile.get(idx).addLast(1);
+    }
+    return;
   }
 
 }
