2b30bb3171256b6a0001abcb64e15100665cd717
Heejeong01110 <123rkdrkd@naver.com>
2025-03-31 17:35:17 +0900

    컬러볼/정답

--- src/main/java/com/example/baekjoon/q10800.java
+++ src/main/java/com/example/baekjoon/q10800.java
@@ -3,16 +3,13 @@ package com.example.baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class q10800 {
 
-  private static int N, M;
+  private static int N;
   private static int[][] inp;
-  private static Map<Integer, ArrayList<Integer>> map;
 
   public static void main(String[] args) throws IOException {
     run();
