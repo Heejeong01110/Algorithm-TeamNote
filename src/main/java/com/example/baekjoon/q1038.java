8de400e90169033b6f490e98e20eb1e57842008e
Heejeong01110 <123rkdrkd@naver.com>
2025-07-10 18:45:40 +0900

    감소하는 수/정답

---
+++ src/main/java/com/example/baekjoon/q1038.java
@@ -0,0 +1,65 @@
+package com.example.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class q1038 {
+
+  private static int N;
+  private static long[] memo;
+
+  public static void main(String[] args) throws IOException {
+    run();
+  }
+
+  public static void run() throws IOException {
+    inputData();
+    System.out.print(Solution());
+  }
+
+  private static void inputData() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    N = Integer.parseInt(br.readLine());
+    memo = new long[N + 1];
+    br.close();
+  }
+
+  private static long Solution() {
+    if (N < 10) {
+      return N;
+    }
+
+    for (int a = 0; a <= 9; a++) {
+      memo[a] = a;
+    }
+
+    int start = 0;
+    int end = 10;
+    while (end <= N) {
+      int cnt = 0;
+
+      for (int i = start; i <= end; i++) {
+        for (int j = 0; j <= 9; j++) {
+          if (memo[i] % 10 <= j) {
+            break;
+          }
+          long num = memo[i] * 10 + j;
+          memo[end + cnt] = num;
+          if (end + cnt == N) {
+            return memo[end + cnt];
+          }
+          cnt++;
+        }
+      }
+      if (cnt == 0) {
+        return -1;
+      }
+      start = end + 1;
+      end += cnt;
+    }
+
+    return -1;
+  }
+
+}
---
+++ src/test/java/com/example/baekjoon/q1038Test.java
@@ -0,0 +1,18 @@
+package com.example.baekjoon;
+
+import org.junit.jupiter.api.Test;
+
+class q1038Test extends AbstractTest {
+
+  @Test
+  void testSample() throws Exception {
+    defaultClassName = "q1038";
+
+    testCorrect("120", "843");
+    testCorrect("18", "42");
+    testCorrect("0", "0");
+    testCorrect("500000", "-1");
+  }
+
+
+}
