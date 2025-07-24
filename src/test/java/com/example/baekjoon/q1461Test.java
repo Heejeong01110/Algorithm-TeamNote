a49e1678f428e807ec0d03e2c9c67c2758de2470
Heejeong01110 <123rkdrkd@naver.com>
2025-07-24 18:38:05 +0900

    AsrTest 실행

--- src/test/java/com/example/baekjoon/q1461Test.java
+++ src/test/java/com/example/baekjoon/q1461Test.java
@@ -1,20 +1,39 @@
 package com.example.baekjoon;
 
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
 
-class q1461Test extends AbstractTest {
+class q1461Test extends AsrTest {
 
-  @Test
-  void testSample() throws Exception {
-    testCorrect("7 2\n"
-        + "-37 2 -6 -39 -29 11 -28", "131");
-    testCorrect("8 3\n"
-        + "-18 -9 -4 50 22 -26 40 -45", "158");
-    testCorrect("6 2\n"
-        + "3 4 5 6 11 -1", "29");
-    testCorrect("1 50\n"
-        + "1", "1");
+  public static Stream<TestCase> generateTestCases() {
+    Stream<TestCase> direct = Stream.of(
+        new TestCase("7 2\n-37 2 -6 -39 -29 11 -28", "131"),
+        new TestCase("8 3\n-18 -9 -4 50 22 -26 40 -45", "158")
+    );
+    return Stream.concat(direct, parseTestCasesFromRaw(rawExampleData()));
   }
 
-
+  protected static String rawExampleData() {
+    return """
+        예제 입력 1\s
+           7 2
+           -37 2 -6 -39 -29 11 -28
+           예제 출력 1\s
+           131
+           예제 입력 2\s
+           8 3
+           -18 -9 -4 50 22 -26 40 -45
+           예제 출력 2\s
+           158
+           예제 입력 3\s
+           6 2
+           3 4 5 6 11 -1
+           예제 출력 3\s
+           29
+           예제 입력 4\s
+           1 50
+           1
+           예제 출력 4\s
+           1
+        """;
+  }
 }
