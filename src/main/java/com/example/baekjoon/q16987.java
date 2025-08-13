d6f4d5c3aab320c168f64cc740226103f64a0d91
Heejeong01110 <123rkdrkd@naver.com>
2025-08-13 22:04:48 +0900

    계란으로 계란치기

---
+++ src/main/java/com/example/baekjoon/q16987.java
@@ -0,0 +1,38 @@
+package com.example.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class q16987 {
+
+  private static int N, M;
+  private static int[] inp;
+
+  public static void main(String[] args) throws IOException {
+    inputData();
+    System.out.print(Solution());
+  }
+
+  private static void inputData() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+    inp = new int[N];
+
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < N; i++) {
+      inp[i] = Integer.parseInt(st.nextToken());
+    }
+
+    br.close();
+  }
+
+  private static int Solution() {
+    return 0;
+  }
+
+}
