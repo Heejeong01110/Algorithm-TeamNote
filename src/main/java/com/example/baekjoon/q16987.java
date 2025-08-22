4efa15a74e4d061d668b608f3a126d7c49e6a034
Heejeong01110 <123rkdrkd@naver.com>
2025-08-22 12:07:40 +0900

    계란으로 계란치기/정답

--- src/main/java/com/example/baekjoon/q16987.java
+++ src/main/java/com/example/baekjoon/q16987.java
@@ -7,32 +7,67 @@ import java.util.StringTokenizer;
 
 public class q16987 {
 
-  private static int N, M;
-  private static int[] inp;
+  static int N;
+  static int[] dura;
+  static int[] weight;
+  static int max = Integer.MIN_VALUE;
 
   public static void main(String[] args) throws IOException {
-    inputData();
-    System.out.print(Solution());
-  }
-
-  private static void inputData() throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+    N = Integer.parseInt(br.readLine());
+    dura = new int[N];
+    weight = new int[N];
 
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    N = Integer.parseInt(st.nextToken());
-    M = Integer.parseInt(st.nextToken());
-    inp = new int[N];
-
-    st = new StringTokenizer(br.readLine());
     for (int i = 0; i < N; i++) {
-      inp[i] = Integer.parseInt(st.nextToken());
+      st = new StringTokenizer(br.readLine(), " ");
+      dura[i] = Integer.parseInt(st.nextToken()); // 계란의 내구도
+      weight[i] = Integer.parseInt(st.nextToken()); // 계란의 무게
     }
 
-    br.close();
+    bt(0, 0); // 0번째 계란부터 시작 , 이 땐 깨진 계란 0개
+
+    System.out.println(max);
   }
 
-  private static int Solution() {
-    return 0;
+  static void bt(int idx, int cnt) {
+    if (idx == N) {
+      // 최댓값 갱신
+      max = Math.max(max, cnt);
+      return;
+    }
+    if (dura[idx] <= 0 || cnt == N - 1) {
+      bt(idx + 1, cnt);
+      return;
+    }
+    int nCnt = cnt;
+    for (int i = 0; i < N; i++) {
+      if (i == idx) {
+        continue;
+      }
+      if (dura[i] <= 0) {
+        continue;
+      }
+      hitEgg(idx, i);
+      if (dura[idx] <= 0) {
+        cnt++;
+      }
+      if (dura[i] <= 0) {
+        cnt++;
+      }
+      bt(idx + 1, cnt);
+      recoveryEgg(idx, i);
+      cnt = nCnt;
+    }
   }
 
+  static void hitEgg(int handEgg, int targetEgg) {
+    dura[targetEgg] -= weight[handEgg];
+    dura[handEgg] -= weight[targetEgg];
+  }
+
+  static void recoveryEgg(int handEgg, int targetEgg) {
+    dura[targetEgg] += weight[handEgg];
+    dura[handEgg] += weight[targetEgg];
+  }
 }
