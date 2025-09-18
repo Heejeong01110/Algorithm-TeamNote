f0a7e00ac1dd5403d3e919c5ebe3569bfbfb7316
Heejeong01110 <123rkdrkd@naver.com>
2025-09-18 20:19:27 +0900

    소트/정답

---
+++ src/main/java/com/example/baekjoon/q1083.java
@@ -0,0 +1,76 @@
+package com.example.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class q1083 {
+
+  static int N, S;
+  static int[] Arr;
+
+  public static void main(String[] args) throws IOException {
+    input();
+    sort();
+    print();
+  }
+
+  public static void input() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    N = Integer.parseInt(br.readLine());
+
+    Arr = new int[N];
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < N; i++) {
+      Arr[i] = Integer.parseInt(st.nextToken());
+    }
+
+    S = Integer.parseInt(br.readLine());
+  }
+
+  public static void sort() {
+    Comparator<int[]> comp = new Comparator<int[]>() {
+      public int compare(int[] o1, int[] o2) {
+        return o2[0] - o1[0];
+      }
+    };
+
+    for (int i = 0; i < N && S > 0; i++) {
+      PriorityQueue<int[]> num = new PriorityQueue<>(comp); // [숫자,인덱스]
+      for (int j = i; j < N; j++) {
+        num.add(new int[]{Arr[j], j});
+      }
+
+      while (!num.isEmpty()) {
+        int targetIndex = num.poll()[1];
+        int swapCnt = targetIndex - i;
+        if (S >= swapCnt) {
+          if (swapCnt > 0) {
+            pull(i, targetIndex);
+            S -= swapCnt;
+          }
+          break;
+        }
+      }
+    }
+  }
+
+  public static void pull(int to, int from) {
+    int tmp = Arr[from];
+    for (int i = from; i > to; i--) {
+      Arr[i] = Arr[i - 1];
+    }
+    Arr[to] = tmp;
+  }
+
+  public static void print() {
+    StringBuilder sb = new StringBuilder();
+    for (int e : Arr) {
+      sb.append(e).append(' ');
+    }
+    System.out.println(sb);
+  }
+}
