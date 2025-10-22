acf48412d9417e32f017c9ef63c317c671ff474a
Heejeong01110 <123rkdrkd@naver.com>
2025-10-22 18:21:16 +0900

    01타일/정답

---
+++ src/main/java/com/example/baekjoon/q1904.java
@@ -0,0 +1,26 @@
+package com.example.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class q1904 {
+
+  public static void main(String[] args) throws IOException {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    int MOD = 15746;
+    br.close();
+
+    int[] dp = new int[1_000_001];
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for (int i = 3; i <= N; i++) {
+      dp[i] = (dp[i - 2] + dp[i - 1]) % MOD;
+    }
+    System.out.print(dp[N]);
+  }
+
+}
---
+++ src/test/java/com/example/baekjoon/q1904Test.java
@@ -0,0 +1,23 @@
+package com.example.baekjoon;
+
+import java.util.stream.Stream;
+
+class q1904Test extends AsrTest {
+
+  public static Stream<TestCase> generateTestCases() {
+    Stream<TestCase> direct = Stream.of(
+    );
+    return Stream.concat(direct, parseTestCasesFromRaw(rawExampleData()));
+  }
+
+  protected static String rawExampleData() {
+    return """
+        예제 입력 1\s
+        4
+        예제 출력 1\s
+        5
+        """;
+  }
+
+
+}
