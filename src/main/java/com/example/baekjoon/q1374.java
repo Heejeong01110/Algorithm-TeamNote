a6bbc103ff35d5f21ed330e192425b01cc92f914
Heejeong01110 <123rkdrkd@naver.com>
2025-11-08 15:26:08 +0900

    수정

--- src/main/java/com/example/baekjoon/q1374.java
+++ src/main/java/com/example/baekjoon/q1374.java
@@ -12,49 +12,46 @@ public class q1374 {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int n = Integer.parseInt(br.readLine());
-    Lecture[] arr = new Lecture[n];
+    Node[] arr = new Node[n];
     for (int i = 0; i < n; i++) {
       StringTokenizer st = new StringTokenizer(br.readLine());
       st.nextToken();
       int start = Integer.parseInt(st.nextToken());
       int end = Integer.parseInt(st.nextToken());
-      arr[i] = new Lecture(start, end);
+      arr[i] = new Node(start, end);
     }
     Arrays.sort(arr, (o1, o2) -> o1.start - o2.start);
-    PriorityQueue<Lecture> pq = new PriorityQueue<>();
-    pq.add(arr[0]);
-    for (int i = 1; i < n; i++) {
-      Lecture current = pq.peek();
-      if (current.end <= arr[i].start) {
-        pq.poll();
-        current.end = arr[i].end;
-        pq.add(current);
-      } else {
-        pq.add(arr[i]);
+    PriorityQueue<Node> queue = new PriorityQueue<>((o1, o2) -> {
+      if (o1.end == o2.end) {
+        return o1.start - o2.start;
       }
+      return o1.end - o2.end;
+    });
 
+    queue.add(arr[0]);
+    for (int i = 1; i < n; i++) {
+      Node now = queue.peek();
+      if (now.end <= arr[i].start) {
+        queue.poll();
+        now.end = arr[i].end;
+        queue.add(now);
+      } else {
+        queue.add(arr[i]);
+      }
     }
-    System.out.println(pq.size());
 
+    System.out.println(queue.size());
   }
 
 
-  private static class Lecture implements Comparable<Lecture> {
+  private static class Node {
 
     int start;
     int end;
 
-    public Lecture(int start, int end) {
+    public Node(int start, int end) {
       this.start = start;
       this.end = end;
-    }
-
-    @Override
-    public int compareTo(Lecture o) {
-      if (this.end == o.end) {
-        return this.start - o.start;
-      }
-      return this.end - o.end;
     }
   }
 }
